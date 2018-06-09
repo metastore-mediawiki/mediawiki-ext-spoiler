9a85deb9c87c37291c398f2d1f53486c90146120
Kitsune Solar <kitsune.solar@gmail.com>
2018-06-09 14:22:26 +0300

    Update resource

---
+++ MW_EXT_Spoiler.class.php
@@ -0,0 +1,98 @@
+<?php
+
+/**
+ * Class MW_EXT_Spoiler
+ * ------------------------------------------------------------------------------------------------------------------ */
+
+class MW_EXT_Spoiler {
+
+	/**
+	 * Clear DATA (escape html).
+	 *
+	 * @param $string
+	 *
+	 * @return string
+	 * -------------------------------------------------------------------------------------------------------------- */
+
+	private static function clearData( $string ) {
+		$outString = htmlspecialchars( trim( $string ), ENT_QUOTES );
+
+		return $outString;
+	}
+
+	/**
+	 * Get MediaWiki issue.
+	 *
+	 * @param $string
+	 *
+	 * @return string
+	 * -------------------------------------------------------------------------------------------------------------- */
+
+	private static function getMsgText( $string ) {
+		$outString = wfMessage( 'mw-ext-spoiler-' . $string )->inContentLanguage()->text();
+
+		return $outString;
+	}
+
+	/**
+	 * Register tag function.
+	 *
+	 * @param Parser $parser
+	 *
+	 * @return bool
+	 * @throws MWException
+	 * -------------------------------------------------------------------------------------------------------------- */
+
+	public static function onParserFirstCallInit( Parser $parser ) {
+		$parser->setHook( 'spoiler', __CLASS__ . '::onRenderTag' );
+
+		return true;
+	}
+
+	/**
+	 * Render tag function.
+	 *
+	 * @param $input
+	 * @param array $args
+	 * @param Parser $parser
+	 * @param PPFrame $frame
+	 *
+	 * @return string
+	 * -------------------------------------------------------------------------------------------------------------- */
+
+	public static function onRenderTag( $input, $args = [], Parser $parser, PPFrame $frame ) {
+		// Argument: title.
+		$getTitle = self::clearData( $args['title'] ?? '' ?: '' );
+		$outTitle = empty( $getTitle ) ? self::getMsgText( 'title' ) : $getTitle;
+
+		// Get content.
+		$getContent = trim( $input );
+		$outContent = $parser->recursiveTagParse( $getContent, $frame );
+
+		// Out HTML.
+		$outHTML = '<details class="mw-ext-spoiler">';
+		$outHTML .= '<summary>' . $outTitle . '</summary>';
+		$outHTML .= '<div class="mw-ext-spoiler-body"><div class="mw-ext-spoiler-content">' . $outContent . '</div></div>';
+		$outHTML .= '</details>';
+
+		// Out parser.
+		$outParser = $outHTML;
+
+		return $outParser;
+	}
+
+	/**
+	 * Load resource function.
+	 *
+	 * @param OutputPage $out
+	 * @param Skin $skin
+	 *
+	 * @return bool
+	 * -------------------------------------------------------------------------------------------------------------- */
+
+	public static function onBeforePageDisplay( OutputPage $out, Skin $skin ) {
+		$out->addModuleStyles( array( 'ext.mw.spoiler.styles' ) );
+
+		return true;
+	}
+}
---
+++ README.md
@@ -0,0 +1,27 @@
+# Информация
+
+Блок со скрытой и отображаемой информацией.
+
+## Синтаксис
+
+```
+<spoiler title="[TITLE]">[CONTENT]</spoiler>
+```
+
+## Установка
+
+1. Загрузите папки и файлы в директорию `extensions/MW_EXT_Spoiler`.
+2. В самый низ файла `LocalSettings.php` добавьте строку:
+
+```
+wfLoadExtension( 'MW_EXT_Spoiler' );
+```
+
+## Авторы
+
+- [**Kitsune Solar**](https://kitsune.solar/) - разработчик.
+
+## Ссылки
+
+- [**METASTORE**](https://metastore.pro/) - хранилище открытых проектов [**METADATA**](https://metadata.foundation/).
+- [**METADATA / Foundation**](https://metadata.foundation/) - поддержка и разработка открытых проектов.
